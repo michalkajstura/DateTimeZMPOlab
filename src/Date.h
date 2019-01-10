759011553cfc7043c427fb61d88801002fc50446
michalkaj <kajsturamichal@gmail.com>
2019-01-10 19:07:41 +0100

    init commit

---
+++ src/Date.h
@@ -0,0 +1,10 @@
+#ifndef DATETIME_DATE_H
+#define DATETIME_DATE_H
+
+
+class Date {
+
+};
+
+
+#endif //DATETIME_DATE_H
---
+++ src/TimeStamp.h
@@ -0,0 +1,11 @@
+#ifndef DATETIME_TIMESTAMP_H
+#define DATETIME_TIMESTAMP_H
+
+#include "Date.h"
+
+class TimeStamp : public Date {
+
+};
+
+
+#endif //DATETIME_TIMESTAMP_H
